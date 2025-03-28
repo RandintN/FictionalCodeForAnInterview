ee3c117a38e0c6c7623632716759d96b65f592b7
Robson Cassiano <robson.cassiano@pm.me>
2025-03-28 18:37:17 -0300

    Testes

--- src/test/java/RomanNumeralGeneratorTest.java
+++ src/test/java/RomanNumeralGeneratorTest.java
@@ -1,14 +1,21 @@
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
-
 import static org.assertj.core.api.Assertions.assertThat;
 
-
-public class RomanNumeralGeneratorShould {
+public class RomanNumeralGeneratorTest {
 
     @Test
     public void generate_a_roman_numeral_for_a_given_decimal_number() {
         assertThat(RomanNumeralGenerator.romanFor(1)).isEqualTo("I");
         assertThat(RomanNumeralGenerator.romanFor(2)).isEqualTo("II");
+        assertThat(RomanNumeralGenerator.romanFor(3)).isEqualTo("III");
+        assertThat(RomanNumeralGenerator.romanFor(4)).isEqualTo("IV");
+        assertThat(RomanNumeralGenerator.romanFor(5)).isEqualTo("V");
+        assertThat(RomanNumeralGenerator.romanFor(7)).isEqualTo("VII");
+        assertThat(RomanNumeralGenerator.romanFor(10)).isEqualTo("X");
+        assertThat(RomanNumeralGenerator.romanFor(18)).isEqualTo("XVIII");
+        assertThat(RomanNumeralGenerator.romanFor(30)).isEqualTo("XXX");
+        assertThat(RomanNumeralGenerator.romanFor(30)).isEqualTo("XXX");
+
+        assertThat(RomanNumeralGenerator.romanFor(2687)).isEqualTo("MMDCLXXXVII");
     }
 }
